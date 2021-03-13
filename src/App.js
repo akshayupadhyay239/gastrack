7bfe321a0b23f98612b06f322990fa75648a9a7d
akshayupadhyay <akshayupadhyay239@gmai.com>
2021-03-13 16:20:40 +0530

    Api changes intergrated

--- src/App.js
+++ src/App.js
@@ -10,7 +10,11 @@ class App extends React.Component {
     isloading:false
   }
 
-  clickHandler = ()=>{
+  componentDidMount = () =>{
+    this.clickHandler()
+  }
+
+  /*clickHandler = ()=>{
     this.setState({ isloading:true })
     fetch("https://ethgasstation.info/api/ethgasAPI.json?api-key=7994d7838528df842e2667dfdc3e1f53480a05f5b21a245433939eed40a7")
       .then(response => response.json())
@@ -22,18 +26,26 @@ class App extends React.Component {
             isloading:false
           })
         })
-  }
+  }*/
 
-  clickTest = ()=>{
+  clickHandler = ()=>{
     //https://www.gasnow.org/api/v3/gas/data?utm_source=web
     //https://ethgas.watch/.netlify/functions/gas this does't let you use data
     this.setState({ isloading:true })
     fetch("https://www.gasnow.org/api/v3/gas/data?utm_source=web")
       .then(response => response.json())
-        .then(data => {
-          console.log(data)
+        .then(rdata => {
+          console.log(rdata)
+          var price = rdata.data.gasPrices
+          const c = 1000000000
+          this.setState({
+            gfastest: Math.floor(price.rapid/c),
+            gfast: Math.floor(price.fast/c),
+            gaverage: Math.floor(price.standard/c),
+            isloading:false
+          })
         })
-        this.setState({ isloading:false })
+        
   }
 
   render(){
@@ -44,22 +56,21 @@ class App extends React.Component {
           <div className="fastest">
             <h2>Fastest</h2>
             <h3>{this.state.gfastest} GWei</h3>
-            <p>under 30 sec</p>
+            <p>15 sec</p>
           </div>
           <div className="fast">
             <h2>Fast</h2>
             <h3>{this.state.gfast} GWei</h3>
-            <p>under 2 minute</p>
+            <p>1 minute</p>
           </div>
           <div className="average">
             <h2>Average</h2>
             <h3>{this.state.gaverage} GWei</h3>
-            <p>under 5 minute</p>
+            <p>3 minute</p>
           </div>
         </div>
         {this.state.isloading?<h1>...Loading data</h1>:<h1>latest gas prices</h1>}
         <button onClick={this.clickHandler}>Get prices</button>
-        <button onClick={this.clickTest}>test</button>
       </div>
     );
   }
